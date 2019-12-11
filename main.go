2313ec262e58aaac239f5be0783a367f39d47b7e
Gregor Best <gbe@unobtanium.de>
2019-12-11 20:51:13 +0100

    Gofmt, add a few doc comments

--- main.go
+++ main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-	"log"
-	"image/color"
-	"image/png"
-	"os"
-	"image/draw"
-	"math/rand"
-	"image"
 	"hash/fnv"
+	"image"
+	"image/color"
+	"image/draw"
+	"image/png"
+	"log"
+	"math/rand"
+	"os"
 )
 
+// IntToBytes converts x to a little endian byte slice
 func IntToBytes(x int64) []byte {
 	res := [8]byte{}
 
@@ -22,7 +23,7 @@ func IntToBytes(x int64) []byte {
 }
 
 type MineField struct {
-	seed [16]byte
+	seed      [16]byte
 	threshold uint32
 }
 
@@ -37,6 +38,7 @@ func NewMineField(threshold uint32) (MineField, error) {
 	return m, nil
 }
 
+// IsMineOnLocation returns true if there is a mine in the location indicated by x and y.
 func (m MineField) IsMineOnLocation(x, y int) bool {
 	// Determine wether x, y contains a mine by hashing it with the seed and checking whether it's less than a threshold
 	h := fnv.New32()
@@ -46,10 +48,12 @@ func (m MineField) IsMineOnLocation(x, y int) bool {
 	return (h.Sum32() % m.threshold) == 0
 }
 
+// RenderToImage returns a gray scale image that represents the are of the mine field m as indicated by the rectangle. The returned
+// image is zoomed by a factor of 4. That is, the image is four times as wide and four times as high as rect.
 func (m MineField) RenderToImage(rect image.Rectangle) image.Image {
 	const zoom = 4
 
-	img := image.NewGray(image.Rect(rect.Min.X * zoom, rect.Min.Y * zoom, rect.Max.X * zoom, rect.Max.Y * zoom))
+	img := image.NewGray(image.Rect(rect.Min.X*zoom, rect.Min.Y*zoom, rect.Max.X*zoom, rect.Max.Y*zoom))
 	mine := image.Uniform{color.Black}
 
 	draw.Draw(img, img.Bounds(), &image.Uniform{color.White}, image.ZP, draw.Over)
@@ -57,7 +61,7 @@ func (m MineField) RenderToImage(rect image.Rectangle) image.Image {
 	for y := img.Bounds().Min.Y; y < img.Bounds().Max.Y; y++ {
 		for x := img.Bounds().Min.X; x < img.Bounds().Max.X; x++ {
 			if m.IsMineOnLocation(x, y) {
-				draw.Draw(img, image.Rect(x * zoom, y * zoom, (x+1)*zoom, (y+1) * zoom), &mine, image.ZP, draw.Over)
+				draw.Draw(img, image.Rect(x*zoom, y*zoom, (x+1)*zoom, (y+1)*zoom), &mine, image.ZP, draw.Over)
 			}
 		}
 	}
@@ -66,7 +70,6 @@ func (m MineField) RenderToImage(rect image.Rectangle) image.Image {
 }
 
 /*
-- Implement "is a mine on this position?" based on some hash of seed and position
 - Render field
 - Build flood fill, up to a certain maximum radius
 */
